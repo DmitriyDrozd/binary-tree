28070afba2062b40c2c5b6249fc9d0575af2e73e
Dmitry Drozd <drozd.dzmitryi@gmail.com>
2016-01-31 23:39:26 +0300

    Function insert added.

--- binary-tree.js
+++ binary-tree.js
@@ -6,11 +6,37 @@ class BinaryTree {
 	}
 
 	insert(data) {
+		var nodeNew = new Node(data)
+
+		if (!this.root){
+			this.root = nodeNew;
+			return;
+		}
 		
+		var nodeCurrent = this.root;
+		while (true){
+
+			if (nodeCurrent.data > data) // case of left
+				if (!nodeCurrent.left) {
+					nodeCurrent.left = nodeNew;
+					break;
+				}
+				else 
+					nodeCurrent = nodeCurrent.left;
+
+			else if (nodeCurrent.data < data) // case of right
+				if (!nodeCurrent.right) {
+					nodeCurrent.right = nodeNew;
+					break;
+				}
+				else 
+					nodeCurrent = nodeCurrent.right;
+			// in case if match needless to do anything
+		}
 	}
 
 	contains(data) {
-
+		
 	}
 
 	remove(data) {
