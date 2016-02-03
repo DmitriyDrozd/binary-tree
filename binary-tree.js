f7fc1c137a8ea7d8639917f6ef491a3239e03339
Dmitry Drozd <drozd.dzmitryi@gmail.com>
2016-02-03 18:44:05 +0300

    Function Delete and isEmpty added

--- binary-tree.js
+++ binary-tree.js
@@ -7,15 +7,12 @@ class BinaryTree {
 
 	insert(data) {
 		var nodeNew = new Node(data)
-
 		if (!this.root){
 			this.root = nodeNew;
 			return;
 		}
-		
 		var nodeCurrent = this.root;
 		while (nodeCurrent){
-
 			if (nodeCurrent.data > data) // case of left
 				if (!nodeCurrent.left) {
 					nodeCurrent.left = nodeNew;
@@ -23,7 +20,6 @@ class BinaryTree {
 				}
 				else 
 					nodeCurrent = nodeCurrent.left;
-
 			else if (nodeCurrent.data < data) // case of right
 				if (!nodeCurrent.right) {
 					nodeCurrent.right = nodeNew;
@@ -31,14 +27,14 @@ class BinaryTree {
 				}
 				else 
 					nodeCurrent = nodeCurrent.right;
-			// in case if match needless to do anything
+			// in case of match needless to do anything
 		}
 	}
 
 	contains(data) {		
-		var nodeCurrent = this.root
-		while(nodeCurrent)
-		{
+		var nodeCurrent = this.root;
+		while(nodeCurrent) {
+
 			if (nodeCurrent.data == data)
 				return true;
 			else if (nodeCurrent.data > data)
@@ -50,14 +46,60 @@ class BinaryTree {
 	}
 
 	remove(data) {
-		
+		var nodeCurrent = this.root;
+		var nodePrev = this.root;
+		while (nodeCurrent) {
+			if (nodeCurrent.data == data) {
+				 if (!nodeCurrent.right && !nodeCurrent.left){
+					if (this.root.data == data)
+						this.root = null;
+					else if (nodePrev.right.data == nodeCurrent.data)
+						nodePrev.right = null;
+					else if (nodePrev.left.data == nodeCurrent.data)
+						nodePrev.left = null;
+					break;
+				}
+				else if (!nodeCurrent.right) { 
+					if (nodePrev.left.data == nodeCurrent.data)
+						nodePrev.left = nodeCurrent.left;
+					else if (nodePrev.right.data == nodeCurrent.data)
+						nodePrev.right = nodeCurrent.left;
+					break;
+				}
+				else { // if right daughter exists
+					if (!nodeCurrent.right.left) {
+						nodeCurrent.data = nodeCurrent.right.data;
+						nodeCurrent.right = null;
+					}
+					else {
+						var nodeReplacement = nodeCurrent.right;
+						while (nodeReplacement.left.left)
+							nodeReplacement = nodeReplacement.left;
+						nodeCurrent.data = nodeReplacement.left.data;
+						nodeReplacement.left = null;
+					}
+					break;
+				}
+			}
+			else {
+				nodePrev = nodeCurrent;
+				if (nodeCurrent.data > data) 
+					nodeCurrent = nodeCurrent.left? nodeCurrent.left : false;
+				else if (nodeCurrent.data < data) 
+					nodeCurrent = nodeCurrent.right? nodeCurrent.right : false;
+			}
+		}
 	}
 
 	size() {
+		var count = this.root? 1 : 0;
+		if (!count) return count;
 
+		var nodeCurrent = this.root;
+		while (nodeCurrent)
 	}
 
 	isEmpty() {
-
+		return (!this.root)? true : false;
 	}
 }
